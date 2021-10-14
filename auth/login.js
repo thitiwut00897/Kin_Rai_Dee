ae1135e68d87d0f7391ca8848c323be93275ea00
thitiwut088 <thitiwut00897@gmail.com>
2021-10-14 21:34:38 +0700

    Update login.js

--- auth/login.js
+++ auth/login.js
@@ -34,7 +34,7 @@ export default function Login() {
       
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>ssss!</Text>
       <Button onPress={logInfacebok} title='Login with facebook'/>
     </View>
   );
